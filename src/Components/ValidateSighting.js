5d85cfd9b441978ceeb5656f32b93c8c33ad8166
miipel <mpeltotalo@gmail.com>
2018-02-01 14:21:16 +0200

    Validation works!

--- src/Components/ValidateSighting.js
+++ src/Components/ValidateSighting.js
@@ -2,11 +2,8 @@ import React from 'react';
 
 const validateSighting = (props) => {
   const sightingName = props.text;
-  console.log(sightingName);
   const validSpecies = props.species;
-  console.log(validSpecies);
-  const isValid = validSpecies.reduce((current, next) => next.name === sightingName, false);
-  console.log(isValid);
+  const isValid = validSpecies.reduce((current, next) => next.name === sightingName || current, false);
 
   return (isValid ? <p>Does match!</p> : <p>Does not match any existing species!</p>);
 }
